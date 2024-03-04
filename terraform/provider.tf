7bf0d768b408914c85c6a24941541fda3ffa1f11
xrscode <120644276+xrscode@users.noreply.github.com>
2024-03-04 17:01:52 +0000

    Terraform Default Time Added

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -15,3 +15,9 @@ terraform {
       region = "eu-west-2"
     }
 }
+
+resource "aws_ssm_parameter" "default_start" {
+    name = "/time"
+    type = "String"
+    value = "1970-01-01 00:00:00.000000"
+}
