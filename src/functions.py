9b9b1becdeadcead99e34524e3d2e175a5025de4
xrscode <120644276+xrscode@users.noreply.github.com>
2024-03-04 18:10:49 +0000

    update

--- src/functions.py
+++ src/functions.py
@@ -18,16 +18,21 @@ def read_bucket_names():
     response = s3.get_object(Bucket=bucket_name, Key=object_key)
     data = json.loads(response['Body'].read().decode('utf-8'))['resources']
     bucket_obj = {'ingestion': None, 'process': None, 'storage': None}
+
+    for key in data:
+        print(key['instances'][0]['attributes']['bucket'])
+
     # Creates a list of bucket names.
-    bucket_list = [key['instances'][0]['attributes']['bucket'] for key in data]
-    for bucket in bucket_list:
-        if bucket[0:9] == 'ingestion':
-            bucket_obj['ingestion'] = bucket
-        elif bucket[0:7] == 'process':
-            bucket_obj['process'] = bucket
-        elif bucket[0:7] == 'storage':
-            bucket_obj['storage'] = bucket
-    return bucket_obj
+
+    # bucket_list = [key['instances'][0]['attributes']['bucket'] for key in data]
+    # for bucket in bucket_list:
+    #     if bucket[0:9] == 'ingestion':
+    #         bucket_obj['ingestion'] = bucket
+    #     elif bucket[0:7] == 'process':
+    #         bucket_obj['process'] = bucket
+    #     elif bucket[0:7] == 'storage':
+    #         bucket_obj['storage'] = bucket
+    pass
 
 
-print(read_bucket_names())
+read_bucket_names()
