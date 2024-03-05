7ac4072241a192a4d539e764f85002333cb69964
xrscode <120644276+xrscode@users.noreply.github.com>
2024-03-05 11:22:24 +0000

    update

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+from pprint import pprint
 
 
 def get_bucket_names():
@@ -17,26 +18,28 @@ def get_bucket_names():
     'storage': 'storage-20240304201826546800000002'}
 
     """
-    s3 = boto3.client('s3')
-    bucket_name = "terraform-xrs"
-    object_key = "tf-state"
-    response = s3.get_object(Bucket=bucket_name, Key=object_key)
-    data = json.loads(response['Body'].read().decode(
-        'utf-8'))['resources']
-    bucket_obj = {}
-    for instance in data:
-        try:
-            bucket_name = instance['instances'][0]['attributes']['bucket']
-        except Exception as e:
-            print(e)
-            continue
-        if bucket_name[0:9] == 'ingestion':
-            bucket_obj['ingestion'] = bucket_name
-        elif bucket_name[0:7] == 'process':
-            bucket_obj['process'] = bucket_name
-        elif bucket_name[0:7] == 'storage':
-            bucket_obj['storage'] = bucket_name
-        return bucket_obj
+    # s3 = boto3.client('s3')
+    # bucket_name = "terraform-xrs"
+    # object_key = "tf-state"
+    # response = s3.get_object(Bucket=bucket_name, Key=object_key)
+    # data = json.loads(response['Body'].read().decode(
+    #     'utf-8'))['resources']
+    # bucket_obj = {}
+    # pprint(data)
+    # for instance in data:
+    #     try:
+    #         bucket_name = instance['instances'][0]['attributes']['bucket']
+    #     except Exception as e:
+    #         print(e)
+    #         continue
+    #     if bucket_name[0:9] == 'ingestion':
+    #         bucket_obj['ingestion'] = bucket_name
+    #     elif bucket_name[0:7] == 'process':
+    #         bucket_obj['process'] = bucket_name
+    #     elif bucket_name[0:7] == 'storage':
+    #         bucket_obj['storage'] = bucket_name
+    #     return bucket_obj
+    pass
 
 
 def get_aws_time():
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -14,3 +14,16 @@ resource "aws_s3_bucket" "storage" {
     bucket_prefix = "storage-"
     force_destroy = true
 }
+
+# Output Bucket Names:
+output "ingestion_bucket_name" {
+  value = aws_s3_bucket.ingestion.bucket
+}
+
+output "process_bucket_name" {
+  value = aws_s3_bucket.process.bucket
+}
+
+output "storage_bucket_name" {
+  value = aws_s3_bucket.storage.bucket
+}
