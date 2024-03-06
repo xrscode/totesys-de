91a087762cf3d4e89416dd4e741e60dde649a67e
xrscode <120644276+xrscode@users.noreply.github.com>
2024-03-06 10:19:45 +0000

    update

---
+++ terraform/layers.tf
@@ -0,0 +1,13 @@
+# Define Source of Lambda Layer Code
+data "archive_file" "lambda_layer_functions_zip"{
+    type = "zip"
+    output_path = ".src/lambda_layer.zip"
+    source_dir = ".src"
+}
+
+# Define the AWS Lambda layer
+resource "aws_lambda_layer_version" "layer_one" {
+    filename = data.archive_file.lambda_layer_functions_zip.output_path
+    layer_name = "layer 1"
+    compatible_runtimes = ["python3.12"]
+}
