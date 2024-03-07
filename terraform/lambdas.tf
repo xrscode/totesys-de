69fad82540357d1ac42057a6f358ff1e5eef5758
xrscode <120644276+xrscode@users.noreply.github.com>
2024-03-07 09:16:12 +0000

    .

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -11,11 +11,6 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_ingestion"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
 data "archive_file" "ingestion_zip" {
   type        = "zip"
   source_file = "${path.module}/../lambdas/ingestion.py"
@@ -40,4 +35,26 @@ resource "aws_lambda_function" "ingestion_lambda" {
 #       foo = "bar"
 #     }
 #   }
+}
+
+# Permissions:
+# Create IAM role.
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_ingestion"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+
+# Define policy document allowing S3 write access.
+data "aws_iam_policy_document" "s3_write_policy" {
+  statement {
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::ingestion-*/*"]
+  }
+}
+
+# Attach S3 write policy to IAM role.
+resource "aws_iam_role_policy_attachment" "s3_write_policy_attachment" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"  # or use the ARN of your custom policy
 }
