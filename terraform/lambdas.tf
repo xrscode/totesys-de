cb071e64c64371897912d5f7b582525ef2db43ab
xrscode <120644276+xrscode@users.noreply.github.com>
2024-03-07 09:05:34 +0000

    Added layer to Lambda function

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -29,6 +29,7 @@ resource "aws_lambda_function" "ingestion_lambda" {
   function_name = "ingestion"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
+  layers = [aws_lambda_layer_version.layer_one.arn]
 
   source_code_hash = data.archive_file.ingestion_zip.output_base64sha256
 
