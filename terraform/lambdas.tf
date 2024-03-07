de46f128eaff6449efd566d42faa35fc039b2cfb
xrscode <120644276+xrscode@users.noreply.github.com>
2024-03-07 08:54:50 +0000

    .

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -25,7 +25,7 @@ data "archive_file" "ingestion_zip" {
 resource "aws_lambda_function" "ingestion_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = archive_file.ingestion_zip.output_path
+  filename      = data.archive_file.ingestion_zip.output_path
   function_name = "Ingestion Lambda"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
