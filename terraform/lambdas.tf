393f4d0176acf66b6ef8f09f84cbd1c9340c1c0e
xrscode <120644276+xrscode@users.noreply.github.com>
2024-03-07 08:53:18 +0000

    .

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -11,26 +11,26 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-# resource "aws_iam_role" "iam_for_lambda" {
-#   name               = "iam_for_lambda"
-#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-# }
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
 
-data "archive_file" "lambda" {
+data "archive_file" "ingestion_zip" {
   type        = "zip"
   source_file = "${path.module}/../lambdas/ingestion.py"
   output_path = "${path.module}/../lambdas/ingestion.zip"
 }
 
-resource "aws_lambda_function" "test_lambda" {
+resource "aws_lambda_function" "ingestion_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
+  filename      = archive_file.ingestion_zip.output_path
+  function_name = "Ingestion Lambda"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
 
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  source_code_hash = data.archive_file.ingestion_zip.output_base64sha256
 
   runtime = "python3.12"
 
