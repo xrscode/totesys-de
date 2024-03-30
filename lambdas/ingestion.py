5045b3181221348ec2445b1ba9ae2c8267ce3ae1
xrscode <120644276+xrscode@users.noreply.github.com>
2024-03-30 11:45:34 +0000

    Remove AWS infrasturcture

--- lambdas/ingestion.py
+++ lambdas/ingestion.py
@@ -1,9 +1,8 @@
-# from functions import *
 from functions import *
 
 
 def handler(event, context):
     names = get_bucket_names()
     date = get_aws_time()
-    # data = all_data()
+    data = all_data()
     return names
