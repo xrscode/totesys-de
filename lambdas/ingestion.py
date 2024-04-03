2aee352988d05000d9711061f77bdc805593423b
xrscode <120644276+xrscode@users.noreply.github.com>
2024-04-03 10:52:56 +0100

    .

--- lambdas/ingestion.py
+++ lambdas/ingestion.py
@@ -1,8 +1,9 @@
 from functions import *
 
-async def handler(event, context):
+
+def handler(event, context):
     try:
-        await data = all_data()
-        await create_path_add_file(data, bucket_name=get_bucket_names()['ingestion'])
-    except:
-        return 'Error!'
+        data = all_data()
+        create_path_add_file(data, bucket_name=get_bucket_names()['ingestion'])
+    except Exception as e:
+        return 'Error: {}'.format(e)
