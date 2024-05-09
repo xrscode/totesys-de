bb7b97d67fee611bf6aee3c377da734f89f14683
xrscode <120644276+xrscode@users.noreply.github.com>
2024-05-09 10:02:24 +0100

    Fixed file paths

--- terraform/lambda_transform.tf
+++ terraform/lambda_transform.tf
@@ -18,13 +18,14 @@ resource "aws_lambda_function" "transform_lambda" {
   function_name = "transform"
 # Specify IAM role Lambda function will assume.
 # Defines permissions Lambda function has.
-  role          = aws_iam_role.iam_for_lambda.arn
+# CHANGE CHANGE CHANGE CHANGE *$*%@£%£*$%@£$*@£$%*@£$%*@£$%*£@
+  role          = aws_iam_role.iam_for_ingestion.arn
 # Defines name of handler function.  ingestion.py > def handler()
   handler       = "ingestion.handler"
 # Lambda layers function depends on.  layer one:
   layers = [aws_lambda_layer_version.layer_one.arn]
 # AWS Lambda updates only if code changes.
-  source_code_hash = data.archive_file.ingestion_zip.output_base64sha256
+  source_code_hash = data.archive_file.transform_zip.output_base64sha256
   runtime = "python3.12"
 }
 
