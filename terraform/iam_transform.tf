90c3560c694fa92f6a59752251ba86cbe3679807
xrscode <120644276+xrscode@users.noreply.github.com>
2024-05-14 11:16:34 +0100

    Fixed naming issue with IAM roles

--- terraform/iam_transform.tf
+++ terraform/iam_transform.tf
@@ -1,7 +1,7 @@
 # Creates Permissions for Transform Lambda to work. 
 
 # Create Assume Role Policy Document
-data "aws_iam_policy_document" "assume_role" {
+data "aws_iam_policy_document_transformation" "assume_role" {
   statement {
     effect = "Allow"
 
@@ -15,27 +15,27 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 # Transform Lambda Policies:
-resource "aws_iam_role" "iam_for_transformation" {
+resource "aws_iam_role_transformation" "iam_for_transformation" {
 # Creates IAM role named 'iam_for_transformation'
   name               = "iam_for_transformation"
 # Defines trust policy for this role:
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  assume_role_policy = data.aws_iam_policy_document_transformation.assume_role.json
 }
 
 # Attach S3 write policy to IAM role.
-resource "aws_iam_role_policy_attachment" "s3_write_policy_attachment" {
-  role       = aws_iam_role.iam_for_transformation.name
+resource "aws_iam_role_policy_attachment" "s3_write_transforamtion" {
+  role       = aws_iam_role_transformation.iam_for_transformation.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"  
 }
 
 # Attach Secrets Access policy to IAM role.
-resource "aws_iam_role_policy_attachment" "secret_access_policy" {
-  role       = aws_iam_role.iam_for_transformation.name
+resource "aws_iam_role_policy_attachment" "transformation_secrets_access" {
+  role       = aws_iam_role_transformation.iam_for_transformation.name
   policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
 
 # Attach ssm:PutParameter to IAM role.
-resource "aws_iam_role_policy_attachment" "parameter_access_policy" {
-  role       = aws_iam_role.iam_for_transformation.name
+resource "aws_iam_role_policy_attachment" "transformation_parameter_access_policy" {
+  role       = aws_iam_role_transformation.iam_for_transformation.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
 }
