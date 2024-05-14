4e968acd46f1665a8fe9e65e3a9b8c84f798c518
xrscode <120644276+xrscode@users.noreply.github.com>
2024-05-14 10:55:03 +0100

    Modified ingestion function

--- lambdas/ingestion.py
+++ lambdas/ingestion.py
@@ -3,7 +3,13 @@ from functions import *
 
 def handler(event, context):
     try:
+        # Access data from RDS Database:
         data = all_data()
+    except Exception as e:
+        return 'Error: {}'.format(e)
+    try:
+        # Adding data to S3 Ingestion bucket:
         create_path_add_file(data, bucket_name=get_bucket_names()['ingestion'])
+        return 'Files added to S3 bucket.'
     except Exception as e:
         return 'Error: {}'.format(e)
