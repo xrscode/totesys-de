711771d601a4d1241209bd36d4ef89a04468cfe6
xrscode <120644276+xrscode@users.noreply.github.com>
2024-05-20 10:35:26 +0100

    Updated transform handler function

--- lambdas/transform.py
+++ lambdas/transform.py
@@ -3,10 +3,7 @@ from functions import *
 
 def handler(event, context):
     try:
-        records = event['Records']
-        bucket_process = get_bucket_names()['process']
-        s3.put_object(Key=records, Bucket=bucket_process)
-        return records
+        return process_write(event)
 
     except Exception as e:
         return 'Error: {}'.format(e)
