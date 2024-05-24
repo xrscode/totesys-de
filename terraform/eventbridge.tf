a29ad29454606a8b08c74459ecfe668950ded78a
xrscode <120644276+xrscode@users.noreply.github.com>
2024-05-24 11:44:50 +0100

    Redeploy infrastucture

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -1,45 +1,45 @@
 # Create EventBridge trigger.
-# resource "aws_iam_role" "lambda_ingestion" {
-#   name = "Ingestion_EventBridge"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "lambda.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
+resource "aws_iam_role" "lambda_ingestion" {
+  name = "Ingestion_EventBridge"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
-# # Attaches AWS managed policy for basic Lambda execution:
-# resource "aws_iam_role_policy_attachment" "lambda_ingestion_policy" {
-#   role       = aws_iam_role.lambda_ingestion.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-# }
+# Attaches AWS managed policy for basic Lambda execution:
+resource "aws_iam_role_policy_attachment" "lambda_ingestion_policy" {
+  role       = aws_iam_role.lambda_ingestion.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
-# # Define EventBridge Rule - set to every 15 minutes:
-# resource "aws_cloudwatch_event_rule" "every_15_minutes" {
-#   name                = "every-15-minutes"
-#   schedule_expression = "rate(15 minutes)"
-# }
+# Define EventBridge Rule - set to every 15 minutes:
+resource "aws_cloudwatch_event_rule" "every_15_minutes" {
+  name                = "every-15-minutes"
+  schedule_expression = "rate(15 minutes)"
+}
 
-# # Lambda Permission:
-# # Grants EventBridge permission to invoke Lambda function:
-# resource "aws_lambda_permission" "allow_eventbridge" {
-#   statement_id  = "AllowExecutionFromEventBridge"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.ingestion_lambda.function_name
-#   principal     = "events.amazonaws.com"
-#   source_arn    = aws_cloudwatch_event_rule.every_15_minutes.arn
-# }
+# Lambda Permission:
+# Grants EventBridge permission to invoke Lambda function:
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.ingestion_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.every_15_minutes.arn
+}
 
-# # Links EventBridge rule to Lambda function:
-# resource "aws_cloudwatch_event_target" "invoke_lambda" {
-#   rule      = aws_cloudwatch_event_rule.every_15_minutes.name
-#   target_id = "lambda"
-#   arn       = aws_lambda_function.ingestion_lambda.arn
-# }
+# Links EventBridge rule to Lambda function:
+resource "aws_cloudwatch_event_target" "invoke_lambda" {
+  rule      = aws_cloudwatch_event_rule.every_15_minutes.name
+  target_id = "lambda"
+  arn       = aws_lambda_function.ingestion_lambda.arn
+}
