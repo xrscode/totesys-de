d655b3f8a37c11d58c60ec42911f99f6aaeea008
xrscode <120644276+xrscode@users.noreply.github.com>
2024-05-24 12:25:36 +0100

    Added setup.py to setup AWS ECR

---
+++ .github/workflows/setup.py
@@ -0,0 +1,41 @@
+import os
+import boto3
+
+# Access the AWS_ECR environment variable:
+repo = os.getenv("AWS_ECR") + '/lambda_functions'
+
+
+def check_ecr(repo_name=repo_id):
+    """
+    ARGS:
+    Repo ID.  The AWS account ID associated with registry.
+
+    RETURNS:
+    Checks if repository 'lambda_functions' exists. 
+    If it exists, returns status code 200. 
+    If repository does not exist, creates repository 'lambda_functions'.
+    Returns status code 200.
+    """
+
+    # Establish ECR Client:
+    client = boto3.client('ecr')
+
+    # Check if repository exists:
+    try:
+        response = response = client.describe_repositories(
+            registryId=repo_name,
+            repositoryNames=[
+                'lambda_functions',
+            ])
+        return f"Lambda_functions already exists."
+    except Exception as e:
+        try:
+            response = client.create_repository(
+                registryId=repo_name,
+                repositoryName='lambda_functions')
+            return f"Status: {response['ResponseMetadata']['HTTPStatusCode']}. Repository 'lambda_functions' created."
+        except Exception as e:
+            return e
+
+
+check_ecr()
